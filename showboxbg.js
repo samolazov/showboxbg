f05d615ac17019802503c8d73a4c522761449b4f
samolazov <samolazov@users.noreply.github.com>
2016-12-07 12:30:37 +0300

    Create showboxbg.js

---
+++ showboxbg.js
@@ -0,0 +1,47 @@
+(function showBox() {
+    var images = document.querySelectorAll(".showbox"),
+        showBoxWrap, showBoxLoading, showBoxImage, buttonClose, buttonNext, buttonPrev;
+        
+    if (images[0]) {
+        showBoxWrap = document.createElement("div");
+        showBoxWrap.id = "showBox";
+
+        showBoxLoading = document.createElement("div");
+        showBoxLoading.id = "showBoxLoading";
+        showBoxWrap.appendChild(showBoxLoading);
+
+        showBoxImage = document.createElement("div");
+        showBoxImage.id = "showBoxImage";
+        showBoxWrap.appendChild(showBoxImage);
+
+        buttonClose = document.createElement("span");
+        buttonClose.id = "close";
+        buttonClose.onclick = function () {showBoxWrap.removeAttribute("class")};
+        showBoxWrap.appendChild(buttonClose);
+
+        buttonNext = document.createElement("span");
+        buttonNext.id = "next";
+        showBoxWrap.appendChild(buttonNext);
+
+        buttonPrev = document.createElement("span");
+        buttonPrev.id = "prev";
+        showBoxWrap.appendChild(buttonPrev);
+
+        for (var i = 0; i < images.length; i++) {
+            (function (i) {
+                images[i].onclick = function () {
+                    showImage(i);
+                };
+            })(i);
+        }
+
+        function showImage(i) {
+            showBoxImage.style.backgroundImage = "url('" + images[i].src.replace("800", "1600") + "')";
+            showBoxWrap.className = "visible";
+            buttonNext.onclick = function () {showImage(i < images.length - 1 ? i + 1 : 0)};
+            buttonPrev.onclick = function () {showImage(i > 0 ? i - 1 : images.length - 1)};
+        }
+
+        document.body.appendChild(showBoxWrap);
+    }
+})();
